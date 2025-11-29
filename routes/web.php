1a0f78d890b780b199da3963f27f203c866b0be5
Hasbi Nabil <hasbinabil.work@gmail.com>
2025-11-29 15:47:05 +0700

    feat(wedding): update wedding route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WalletController;
+use App\Http\Controllers\WeddingController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,15 +13,22 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
+    // profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::post('/dashboard/refresh-gold', [DashboardController::class, 'updatePrice'])->name('gold.refresh');
+    Route::post('/dashboard/refresh-gold', [DashboardController::class, 'updatePrice'])->name('dashboard.refreshGold');
 
+    // wallet
     Route::post('/wallet/{wallet}/add-balance', [WalletController::class, 'addBalance'])->name('wallet.addBalance');
     Route::resource('wallet', WalletController::class)->except(['create', 'show', 'edit']);
+
+    // wedding
+    Route::resource('wedding', WeddingController::class)->except(['show', 'edit']);
+    Route::get('/api/users/search', [UserController::class, 'getUser']);
 });
 
 require __DIR__ . '/auth.php';
